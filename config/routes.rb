7e3dc1f680aef65c5a7056b2cdd7080e86658398
kondo0602 <kondo8363@gmail.com>
2021-05-31 08:45:49 +0900

    Aboutページに遷移できない事象の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#Home'
-  get    '/about',  to: 'pages#about'
+  get    '/about',  to: 'pages#About'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
