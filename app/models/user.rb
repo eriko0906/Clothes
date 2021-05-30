9f7cc1a3b5cc616b522dfe5f64ba7e3ca21e633d
kondo0602 <kondo8363@gmail.com>
2021-05-30 09:49:09 +0900

    ユーザ画像のアップロード機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { minimum: 6 }
 
     has_secure_password
+    has_one_attached :image
+
+    def thumbnail
+        if self.image.attached?
+            self.image.variant(combine_options:{gravity: :center, resize:"100x100^",crop:"100x100+0+0"}).processed
+        else
+            '/assets/pui.jpg'
+        end
+    end
 end
